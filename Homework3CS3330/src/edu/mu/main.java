5b4462daf757dc099a79b0529a2a24ea91eef39d
bsimpson0 <91200121+bsimpson0@users.noreply.github.com>
2024-02-14 12:38:26 -0600

    Worked on initializeStock() method (NOT FINISHED)

--- Homework3CS3330/src/edu/mu/main.java
+++ Homework3CS3330/src/edu/mu/main.java
@@ -1,5 +1,12 @@
 package edu.mu;
+import edu.mu.storemanager.StockManagerSingleton;
 
 public class main {
+	
+	public static void main(String[] args) {
+		
+		StockManagerSingleton stock = new StockManagerSingleton();
+		stock.initializeStock();
+	}
 
 }
--- Homework3CS3330/src/edu/mu/storemanager/StockManagerSingleton.java
+++ Homework3CS3330/src/edu/mu/storemanager/StockManagerSingleton.java
@@ -1,13 +1,26 @@
 package edu.mu.storemanager;
+import java.io.File;
 
 public class StockManagerSingleton {
 	
-	String inventoryFilePath = "files/inventory.csv";
-
-
-
-	public boolean initializaitonStock() {
+	public StockManagerSingleton() {
 		
+	}
+	
+	private String inventoryFilePath = "Homework3CS3330/src/files/inventory.csv";
+
+
+
+	public boolean initializeStock() {
+		//check if file exists
+		File file = new File(inventoryFilePath);
+		if(!file.exists()) {
+			System.out.println("Inventory file does not exist");
+			return false;
+		}
+		
+		System.out.println("Inventory file exists!");
+	
 		
 		return false;
 		
