82270a9c49d276d3d2c4cf78c8c18a1783a0738b
bsimpson0 <91200121+bsimpson0@users.noreply.github.com>
2024-02-14 21:49:42 -0600

    Updated initializeStock() to tokenize file (Not done)

--- Homework3CS3330/src/edu/mu/storemanager/StockManagerSingleton.java
+++ Homework3CS3330/src/edu/mu/storemanager/StockManagerSingleton.java
@@ -1,5 +1,8 @@
 package edu.mu.storemanager;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class StockManagerSingleton {
 	
@@ -12,17 +15,69 @@ public class StockManagerSingleton {
 
 
 	public boolean initializeStock() {
-		//check if file exists
-		File file = new File(inventoryFilePath);
-		if(!file.exists()) {
-			System.out.println("Inventory file does not exist");
+		try {
+			//check if file exists
+			File file = new File(inventoryFilePath);
+			if(!file.exists()) {
+				System.out.println("Inventory file does not exist");
+				return false;
+			}
+			
+			System.out.println("Inventory file exists!");
+			
+			Scanner fileIn = new Scanner(new FileInputStream(inventoryFilePath));
+			
+			//skipping first line of file (not actual data)
+			if(fileIn.hasNextLine()) {
+				fileIn.nextLine();
+			}
+			
+			
+			while(fileIn.hasNextLine()) {
+				String inventoryFileLine = fileIn.nextLine();
+				String[] inventoryData = inventoryFileLine.split(","); //splitting line up at commas
+				
+				//Type,Title,Price,Year,Genre -> order of data in each line
+				String type = inventoryData[0];
+				String title = inventoryData[1];
+				double price = Double.parseDouble(inventoryData[2]);
+				int year = Integer.parseInt(inventoryData[3]);
+				Genre genre = Genre.valueOf(inventoryData[4]);
+				
+				//creating MediaProduct object based on type
+				MediaProduct product = null;
+				if(type.equals("CD")) {
+					//product = new CDRecordProduct(title, price, year, genre);
+					System.out.println(type + ", " + title + ", " + price + ", " + year + ", " + genre);
+				}
+				else if(type.equals("Vinyl")) {
+					//product = new VinylRecordProduct(title, price, year, genre);
+					System.out.println(type + ", " + title + ", " + price + ", " + year + ", " + genre);
+				}
+				else if(type.equals("Tape")) {
+					//product = new TapeRecordProduct(title, price, year, genre);
+					System.out.println(type + ", " + title + ", " + price + ", " + year + ", " + genre);
+				}
+				else {
+					System.out.println(type + ", " + title + ", " + price + ", " + year + ", " + genre);
+				}
+				
+				
+				
+				
+			}
+			
+		}
+		
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
 			return false;
 		}
 		
-		System.out.println("Inventory file exists!");
 	
 		
-		return false;
+		
+		return true;
 		
 	}
 
